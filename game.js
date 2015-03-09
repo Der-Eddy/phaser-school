5530bd7a30a5c3065b4f6db49e8ee9513ff53a81
Eduard <dereddyteddy@gmail.com>
2015-03-09 11:14:27 +0100

    Update game.js

--- game.js
+++ game.js
@@ -78,7 +78,7 @@ function create() {
         star.body.bounce.y = 0.7 + Math.random() * 0.2;
     }
 
-    scoreText = game.add.text(16, 16, 'score: 0', { fontSize: '32px', fill: '#000' });
+    scoreText = game.add.text(16, 16, 'score: 0', { fontSize: '32px', fill: '#ffc63e' });
 }
 
 function update() {
